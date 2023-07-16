63455bf0d5443d60261cfc47ea69b593ffcbb847
natsukikokubu <natsukikokubu3@gmail.com>
2023-07-16 21:55:15 +0900

    addInput

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 
-//AppをcreateTodoという名前で定義,引数はvalueと更新するためのsetValue
-const App = ({ createTodo }) => {
+// ppをcreateTodoという名前で定義,引数はvalueと更新するためのsetValue
+const App = () => {
   //Inputに入力された値のStateを管理
   const [value, setValue] = useState("");
   console.log(value);
-  //App関数にデータを渡すためのreturn
-  //複数あるから（）でくくっている,<div>は必要？
+  // App関数にデータを渡すためのreturn
+  // 複数あるから（）でくくっている,<div>は必要？
   return (
     <div>
       <input
